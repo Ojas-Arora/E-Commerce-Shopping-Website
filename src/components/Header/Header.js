1e930e0c38a3bb51f6afcb372865f1bf892f695d
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-11-30 23:06:17 +0530

    done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,7 +27,7 @@ const Header = (prop) => {
   const [search, setSearch] = useState('');
 
   const searchRef = createRef();
-  const bannerMessage = 'Free shipping worldwide';
+  const bannerMessage = 'Welcome to SYDNEY! 🛍️ Your One-Stop Shop';
   const searchSuggestions = [
     'Oversize sweaters',
     'Lama Pajamas',
